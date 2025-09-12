12ad403d17e320325c2caedba3a80406ec6c5320
polodev <polodev10@gmail.com>
2025-09-12 10:41:40 +0600

    added till 30

--- prompt.sh
+++ prompt.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 files=(
-  "prompts-single-file/16-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/17-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/18-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/19-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/20-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/31-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/32-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/33-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/34-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/35-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/36-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/37-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/38-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/39-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/40-learn-vocabulary-through-stories-prompt.md"
 )
 
 for f in "${files[@]}"; do
