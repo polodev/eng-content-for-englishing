fe8c57ec840b71b44caa0386abec3bca7a873d87
polodev <polodev10@gmail.com>
2025-09-13 12:10:15 +0600

    --updated--

--- prompt.sh
+++ prompt.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 files=(
-  "prompts-single-file/23-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/24-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/25-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/26-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/27-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/28-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/29-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/30-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/31-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/32-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/33-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/34-learn-vocabulary-through-stories-prompt.md"
-  "prompts-single-file/35-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/36-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/37-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/38-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/39-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/40-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/41-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/42-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/43-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/44-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/45-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/46-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/47-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/48-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/49-learn-vocabulary-through-stories-prompt.md"
+  "prompts-single-file/50-learn-vocabulary-through-stories-prompt.md"
+ 
 )
 
 for f in "${files[@]}"; do
