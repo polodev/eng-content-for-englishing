ebd832a6cacfaa622eaa3c6f5441c7a6381baff6
polodev <polodev10@gmail.com>
2025-09-15 21:20:59 +0600

    completed till 200

--- prompt.sh
+++ prompt.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 files=(
- "prompts-single-file/072-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/073-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/074-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/075-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/076-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/077-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/078-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/079-learn-vocabulary-through-stories-prompt.md"
- "prompts-single-file/080-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/189-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/190-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/191-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/192-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/193-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/194-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/195-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/196-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/197-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/198-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/199-learn-vocabulary-through-stories-prompt.md"
+ "prompts-single-file/200-learn-vocabulary-through-stories-prompt.md"
 )
 
 for f in "${files[@]}"; do
---
+++ some-links.md
@@ -0,0 +1,2 @@
+https://www.scribd.com/document/696542128/Spoken-English-%E0%A6%9C%E0%A7%9F%E0%A7%87%E0%A6%B0-%E0%A6%B8%E0%A6%B9%E0%A6%9C-%E0%A6%95%E0%A7%8C%E0%A6%B6%E0%A6%B2-Smart-Version
+
